b423eb4421ca1930f5fb5a8e05e956dd14472de8
jakkie <jakubkielbasiewicz@gmail.com>
2022-02-23 21:54:25 +0100

    init token

---
+++ lexer/token.py
@@ -0,0 +1,11 @@
+
+
+class Token:
+    def __init__(self, type, value) -> None:
+        self.type = type
+        self.value = value 
+
+    def __repr__(self) -> str:
+        if self.value:
+            return f"{self.type}:{self.value}"
+        return f"{self.type}"
