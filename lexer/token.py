8fe56b7d024e82f5b78bfd1576d7494f6ae88898
jakkie <jakubkielbasiewicz@gmail.com>
2022-03-01 23:26:34 +0100

    tokens

--- lexer/token.py
+++ lexer/token.py
@@ -15,6 +15,7 @@ class TokenType(IdGenerator):
     SEMICOLON = auto()
     STAR = auto()
     SLASH = auto()
+    DASH = auto()
     LT = auto()
     LTE = auto()
     GT = auto()
@@ -24,6 +25,7 @@ class TokenType(IdGenerator):
     NEQ = auto()
     AND = auto()
     OR = auto()
+    MOD = auto()
 
     SPACE = auto()
     BREAK = auto()
@@ -67,8 +69,10 @@ class TokenType(IdGenerator):
     OBJECT = auto()
     ARRAY = auto()
     GEOGRAPHY = auto()
+    JSON = auto()
 
     ALIAS = auto()
+    ALTER = auto()
     AS = auto()
     ASC = auto()
     BETWEEN = auto()
@@ -114,6 +118,7 @@ class TokenType(IdGenerator):
     OUTER = auto()
     OVER = auto()
     PARTITION = auto()
+    PARTITION_BY = auto()
     QSTRING = auto()
     DQSTRING = auto()
     RIGHT = auto()
@@ -134,192 +139,10 @@ class TokenType(IdGenerator):
     WITH = auto()
 
 
-
-# current elem has to hold possible ways to use next
-
-
-class TokenType(AutoName):
-    L_PAREN = auto()
-    R_PAREN = auto()
-    L_BRACKET = auto()
-    R_BRACKET = auto()
-    L_BRACE = auto()
-    R_BRACE = auto()
-    COMMA = auto()
-    DOT = auto()
-    DASH = auto()
-    PLUS = auto()
-    COLON = auto()
-    DCOLON = auto()
-    SEMICOLON = auto()
-    STAR = auto()
-    SLASH = auto()
-    LT = auto()
-    LTE = auto()
-    GT = auto()
-    GTE = auto()
-    NOT = auto()
-    EQ = auto()
-    NEQ = auto()
-    AND = auto()
-    OR = auto()
-    AMP = auto()
-    DPIPE = auto()
-    PIPE = auto()
-    CARET = auto()
-    TILDA = auto()
-    LSHIFT = auto()
-    RSHIFT = auto()
-    LAMBDA = auto()
-
-    SPACE = auto()
-    BREAK = auto()
-
-    STRING = auto()
-    NUMBER = auto()
-    IDENTIFIER = auto()
-    COLUMN = auto()
-    COLUMN_DEF = auto()
-    SCHEMA = auto()
-    TABLE = auto()
-    VAR = auto()
-
-    # types
-    BOOLEAN = auto()
-    TINYINT = auto()
-    SMALLINT = auto()
-    INT = auto()
-    BIGINT = auto()
-    FLOAT = auto()
-    DOUBLE = auto()
-    DECIMAL = auto()
-    CHAR = auto()
-    VARCHAR = auto()
-    TEXT = auto()
-    BINARY = auto()
-    JSON = auto()
-    TIMESTAMP = auto()
-    TIMESTAMPTZ = auto()
-    DATE = auto()
-
-    # keywords
-    ADD_FILE = auto()
-    ALIAS = auto()
-    ALL = auto()
-    ALTER = auto()
-    ARRAY = auto()
-    ASC = auto()
-    AUTO_INCREMENT = auto()
-    BETWEEN = auto()
-    BUCKET = auto()
-    BY = auto()
-    CACHE = auto()
-    UNCACHE = auto()
-    CASE = auto()
-    CAST = auto()
-    CHARACTER_SET = auto()
-    COUNT = auto()
-    COLLATE = auto()
-    COMMENT = auto()
-    COMMENT_END = auto()
-    COMMENT_START = auto()
-    CREATE = auto()
-    CROSS = auto()
-    CURRENT_ROW = auto()
-    DIV = auto()
-    DEFAULT = auto()
-    DELETE = auto()
-    DESC = auto()
-    DISTINCT = auto()
-    DROP = auto()
-    ELSE = auto()
-    END = auto()
-    ENGINE = auto()
-    EXCEPT = auto()
-    EXISTS = auto()
-    EXPLAIN = auto()
-    EXTRACT = auto()
-    FALSE = auto()
-    FOLLOWING = auto()
-    FORMAT = auto()
-    FULL = auto()
-    FROM = auto()
-    GROUP = auto()
-    HAVING = auto()
-    HINT = auto()
-    IF = auto()
-    ILIKE = auto()
-    IN = auto()
-    INNER = auto()
-    INSERT = auto()
-    INTERSECT = auto()
-    INTERVAL = auto()
-    INTO = auto()
-    IS = auto()
-    JOIN = auto()
-    LATERAL = auto()
-    LAZY = auto()
-    LEFT = auto()
-    LIKE = auto()
-    LIMIT = auto()
-    MAP = auto()
-    MOD = auto()
-    NULL = auto()
-    ON = auto()
-    OPTIMIZE = auto()
-    OPTIONS = auto()
-    ORDER = auto()
-    ORDERED = auto()
-    ORDINALITY = auto()
-    OUTER = auto()
-    OUT_OF = auto()
-    OVER = auto()
-    OVERWRITE = auto()
-    PARTITION = auto()
-    PARTITION_BY = auto()
-    PERCENT = auto()
-    PRECEDING = auto()
-    PRIMARY_KEY = auto()
-    RANGE = auto()
-    RECURSIVE = auto()
-    REPLACE = auto()
-    RIGHT = auto()
-    RLIKE = auto()
-    ROWS = auto()
-    SCHEMA_COMMENT = auto()
-    SELECT = auto()
-    SET = auto()
-    SHOW = auto()
-    STORED = auto()
-    TABLE_SAMPLE = auto()
-    TEMPORARY = auto()
-    TIME = auto()
-    THEN = auto()
-    TRUE = auto()
-    TRUNCATE = auto()
-    TRY_CAST = auto()
-    UNBOUNDED = auto()
-    UNION = auto()
-    UNNEST = auto()
-    UPDATE = auto()
-    VALUES = auto()
-    VIEW = auto()
-    WHEN = auto()
-    WHERE = auto()
-    WITH = auto()
-    WITHOUT = auto()
-    ZONE = auto()
-
 class Tokenizer:
     SINGLE_TOKENS = {
-        "(": TokenType.L_PAREN,
-        ")": TokenType.R_PAREN,
-        "[": TokenType.L_BRACKET,
-        "]": TokenType.R_BRACKET,
-        "{": TokenType.L_BRACE,
-        "}": TokenType.R_BRACE,
-        "&": TokenType.AMP,
-        "^": TokenType.CARET,
+        "(": TokenType.LEFTPAREN,
+        ")": TokenType.RIGHTPAREN,
         ":": TokenType.COLON,
         ",": TokenType.COMMA,
         ".": TokenType.DOT,
@@ -329,77 +152,47 @@ class Tokenizer:
         "<": TokenType.LT,
         "%": TokenType.MOD,
         "!": TokenType.NOT,
-        "|": TokenType.PIPE,
         "+": TokenType.PLUS,
         ";": TokenType.SEMICOLON,
         "/": TokenType.SLASH,
         "*": TokenType.STAR,
-        "~": TokenType.TILDA,
     }
 
     KEYWORDS = {
-        "/*+": TokenType.HINT,
         "--": TokenType.COMMENT,
         "/*": TokenType.COMMENT_START,
         "*/": TokenType.COMMENT_END,
         "==": TokenType.EQ,
-        "::": TokenType.DCOLON,
-        "||": TokenType.DPIPE,
         ">=": TokenType.GTE,
         "<=": TokenType.LTE,
         "<>": TokenType.NEQ,
         "!=": TokenType.NEQ,
-        "<<": TokenType.LSHIFT,
-        ">>": TokenType.RSHIFT,
-        "->": TokenType.LAMBDA,
-        "ADD ARCHIVE": TokenType.ADD_FILE,
-        "ADD ARCHIVES": TokenType.ADD_FILE,
-        "ADD FILE": TokenType.ADD_FILE,
-        "ADD FILES": TokenType.ADD_FILE,
-        "ADD JAR": TokenType.ADD_FILE,
-        "ADD JARS": TokenType.ADD_FILE,
-        "ALL": TokenType.ALL,
         "ALTER": TokenType.ALTER,
         "AND": TokenType.AND,
         "ASC": TokenType.ASC,
         "AS": TokenType.ALIAS,
-        "AUTO_INCREMENT": TokenType.AUTO_INCREMENT,
         "BETWEEN": TokenType.BETWEEN,
-        "BUCKET": TokenType.BUCKET,
         "BY": TokenType.BY,
-        "CACHE": TokenType.CACHE,
-        "UNCACHE": TokenType.UNCACHE,
         "CASE": TokenType.CASE,
         "CAST": TokenType.CAST,
-        "CHARACTER SET": TokenType.CHARACTER_SET,
-        "COLLATE": TokenType.COLLATE,
-        "COMMENT": TokenType.SCHEMA_COMMENT,
         "COUNT": TokenType.COUNT,
         "CREATE": TokenType.CREATE,
         "CROSS": TokenType.CROSS,
-        "CURRENT ROW": TokenType.CURRENT_ROW,
-        "DIV": TokenType.DIV,
-        "DEFAULT": TokenType.DEFAULT,
         "DELETE": TokenType.DELETE,
         "DESC": TokenType.DESC,
         "DISTINCT": TokenType.DISTINCT,
         "DROP": TokenType.DROP,
         "ELSE": TokenType.ELSE,
         "END": TokenType.END,
-        "ENGINE": TokenType.ENGINE,
         "EXCEPT": TokenType.EXCEPT,
         "EXISTS": TokenType.EXISTS,
-        "EXPLAIN": TokenType.EXPLAIN,
         "EXTRACT": TokenType.EXTRACT,
         "FALSE": TokenType.FALSE,
-        "FORMAT": TokenType.FORMAT,
         "FULL": TokenType.FULL,
-        "FOLLOWING": TokenType.FOLLOWING,
         "FROM": TokenType.FROM,
         "GROUP BY": TokenType.GROUP,
         "HAVING": TokenType.HAVING,
         "IF": TokenType.IF,
-        "ILIKE": TokenType.ILIKE,
         "IN": TokenType.IN,
         "INNER": TokenType.INNER,
         "INSERT": TokenType.INSERT,
@@ -409,48 +202,28 @@ class Tokenizer:
         "IS": TokenType.IS,
         "JOIN": TokenType.JOIN,
         "LATERAL": TokenType.LATERAL,
-        "LAZY": TokenType.LAZY,
         "LEFT": TokenType.LEFT,
         "LIKE": TokenType.LIKE,
         "LIMIT": TokenType.LIMIT,
         "NOT": TokenType.NOT,
         "NULL": TokenType.NULL,
         "ON": TokenType.ON,
-        "OPTIMIZE": TokenType.OPTIMIZE,
-        "OPTIONS": TokenType.OPTIONS,
         "OR": TokenType.OR,
         "ORDER BY": TokenType.ORDER,
-        "ORDINALITY": TokenType.ORDINALITY,
         "OUTER": TokenType.OUTER,
-        "OUT OF": TokenType.OUT_OF,
         "OVER": TokenType.OVER,
-        "OVERWRITE": TokenType.OVERWRITE,
         "PARTITION": TokenType.PARTITION,
         "PARTITION BY": TokenType.PARTITION_BY,
-        "PERCENT": TokenType.PERCENT,
-        "PRECEDING": TokenType.PRECEDING,
-        "PRIMARY KEY": TokenType.PRIMARY_KEY,
-        "RANGE": TokenType.RANGE,
-        "RECURSIVE": TokenType.RECURSIVE,
-        "REGEXP": TokenType.RLIKE,
-        "REPLACE": TokenType.REPLACE,
         "RIGHT": TokenType.RIGHT,
-        "RLIKE": TokenType.RLIKE,
-        "ROWS": TokenType.ROWS,
         "SELECT": TokenType.SELECT,
         "SET": TokenType.SET,
         "SHOW": TokenType.SHOW,
-        "STORED": TokenType.STORED,
         "TABLE": TokenType.TABLE,
-        "TABLESAMPLE": TokenType.TABLE_SAMPLE,
-        "TEMP": TokenType.TEMPORARY,
-        "TEMPORARY": TokenType.TEMPORARY,
         "THEN": TokenType.THEN,
         "TIME": TokenType.TIME,
         "TRUE": TokenType.TRUE,
         "TRUNCATE": TokenType.TRUNCATE,
         "TRY_CAST": TokenType.TRY_CAST,
-        "UNBOUNDED": TokenType.UNBOUNDED,
         "UNION": TokenType.UNION,
         "UNNEST": TokenType.UNNEST,
         "UPDATE": TokenType.UPDATE,
@@ -459,8 +232,6 @@ class Tokenizer:
         "WHEN": TokenType.WHEN,
         "WHERE": TokenType.WHERE,
         "WITH": TokenType.WITH,
-        "WITHOUT": TokenType.WITHOUT,
-        "ZONE": TokenType.ZONE,
         "ARRAY": TokenType.ARRAY,
         "BOOL": TokenType.BOOLEAN,
         "BOOLEAN": TokenType.BOOLEAN,
@@ -476,7 +247,6 @@ class Tokenizer:
         "BIGINT": TokenType.BIGINT,
         "INT8": TokenType.BIGINT,
         "DECIMAL": TokenType.DECIMAL,
-        "MAP": TokenType.MAP,
         "NUMERIC": TokenType.DECIMAL,
         "FIXED": TokenType.DECIMAL,
         "REAL": TokenType.FLOAT,
@@ -491,7 +261,7 @@ class Tokenizer:
         "TEXT": TokenType.TEXT,
         "BINARY": TokenType.BINARY,
         "TIMESTAMP": TokenType.TIMESTAMP,
-        "TIMESTAMPTZ": TokenType.TIMESTAMPTZ,
+        "TIMESTAMPTZ": TokenType.TIMESTAMP_TZ,
         "DATE": TokenType.DATE,
     }
 
@@ -505,9 +275,6 @@ class Tokenizer:
 
     COMMANDS = {
         TokenType.ALTER,
-        TokenType.ADD_FILE,
-        TokenType.EXPLAIN,
-        TokenType.OPTIMIZE,
         TokenType.SET,
         TokenType.SHOW,
         TokenType.TRUNCATE,
